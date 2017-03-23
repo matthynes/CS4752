194d6a54efec2c723bed875aac33f252dd36cd62
matt <matthynesdev@gmail.com>
2017-03-23 19:54:22 -0230

    Documentation

--- Assignment 4/ga_student.py
+++ Assignment 4/ga_student.py
@@ -64,6 +64,7 @@ class GASettings:
 
 
 # TODO: Modify this function (optional)
+# NOTE: Consult README.txt for experimentation results.
 #
 # The function which returns the settings you will use back to the main function,
 # these will then be used by evolve() below. Try and tweak the settings so that 
@@ -74,9 +75,9 @@ def get_ga_settings(sudoko_size):
     settings.individual_values = [(i + 1) for i in range(sudoko_size)]  # list of possible values individuals can take
     settings.individual_size = sudoko_size * sudoko_size  # length of an individual
     settings.fitness_function = eval_sudoku  # the fitness function of an individual
-    settings.population_size = 100  # total size of each population                             (experiment with this)
-    settings.elitism_ratio = 0.2  # select top x% of individuals to survive                   (experiment with this)
-    settings.parent_roulette_ratio = 0.2  # select x% of population as parents via roulette wheel     (experiment
+    settings.population_size = 120  # total size of each population                             (experiment with this)
+    settings.elitism_ratio = 0.4  # select top x% of individuals to survive                   (experiment with this)
+    settings.parent_roulette_ratio = 0.1  # select x% of population as parents via roulette wheel     (experiment
     # with this)
     settings.mutation_rate = 0.2  # mutation rate percentage                                  (experiment with this)
     settings.crossover_index = settings.individual_size // 2  # the index to split parents for recombination
@@ -84,24 +85,8 @@ def get_ga_settings(sudoko_size):
     return settings
 
 
-# Student TODO: Implement This Function (required)
-#
-# This is the main function that you must implement for Assignment 4
-# Follow the class notes and the pseudocode below to implement a one-generation update
-# of a genetic algorithm. What you must include is listed in the algorithm below.
-#
-# args:
-#
-#   population - A list of individuals, each of which are a 1D list representing a 'flattened'
-#                sudoku candidate solution. 
-#
-#   settings   - An instance of a GAsettings object which uses the settings within it to compute
-#                the next generation's population
-#
-# returns:
-#
-#   next_population - The next population after a one-generation genetic algorithm evolution
-#
+# weighted random selection
+# returns the index of the chosen individual
 def roulette_select(population):
     max = sum(population)
     select = random.uniform(0, max)
@@ -115,8 +100,6 @@ def roulette_select(population):
 def evolve(population, settings):
     # 1. First, it is very useful to store the fitness of each of the individuals in a separate
     # array, pop_fitness, such that pop_fitness[i] = fitness(population[i])
-    #
-    # pop_fitness = [calculate fitnesses of population]
     pop_fitness = [settings.fitness_function(x) for x in population]
 
     # 2. The next step is to select the parents that will be used to generate the next generation's
@@ -127,76 +110,67 @@ def evolve(population, settings):
     parents = []
     while len(parents) < P:
         parents.append(population[roulette_select(pop_fitness)])
-    #
-    #
+
     # 3. Let's now define a blank list which will hold the next population's individuals. At the
     # end of this function, it should be the same size as the previous population.
-    #
     next_population = []
-    #
-    #
+
     # 4. The next step is elite individual selection. You will select the top E fitness individuals
     # from input population and insert them into next_population, since they are elite, and chosen
     # to survive. The number E is equal to int(settings.elitism_ratio * len(population))
     # For example, if E is 2, and pop_fitness = [5, 3, 1, 7] then you will choose the first and last
     # individuals as the elite ones to add to the next population, since they have the top 2 fitnesses.
     E = int(settings.elitism_ratio * len(population))
+    # sorts pop_fitness and transforms fitnesses into their indices then returns the last E individuals
     elite = sorted(range(len(pop_fitness)), key=lambda x: pop_fitness[x])[-E:]
+    elite = [population[e] for e in elite]
     next_population.extend(elite)
-    #
+
     # 5. Step 5 is the offspring generation (recombination) step. You will generate offspring until
     # you have enough to fill next_population to the size of the previous population. You will generate
     # offspring via crossover, with the # crossover index being defined in settings.crossover_index.
     # Pseudocode for generating children:
-    #
     offspring = []
-    while len(next_population) < len(population):
+    while len(offspring) < len(population) - len(next_population):
         mother = random.choice(parents)
         father = random.choice(parents)
 
         if mother == father:
             continue
 
+        # crossover first part of mother with second part of father
         child1 = mother[:settings.crossover_index] + father[settings.crossover_index:]
+        # crossover first part of father with second part of mother
         child2 = father[:settings.crossover_index] + mother[settings.crossover_index:]
 
         offspring.extend([child1, child2])
 
-        # mother = choose random parent from parent list
-        # father = choose random parent from parent list
-        #   if (mother == father): continue
-        #   child1 = crossover 1st part of mother with 2nd part of father
-        #   child2 = crossover 2nd part of father with 1st part of mother
-        #   add children to the offspring list
+    # 6. Step 6 is to mutate the offspring. You will mutate M of the offpspring, where M is defined
+    # as int(settings.mutation_rate * len(offspring)). Do not mutate a single offspring twice. You
+    # will perform mutation by changing a single element of the individual's array to a random legal
+    # sudoku number, which are given in settings.individual_values
+    M = int(settings.mutation_rate * len(offspring))
+    mutated = set()
 
+    for i in range(M):
+        # i = random.randint(0, len(offspring) - 1)
+        child = offspring[i]
+        j = random.randint(0, len(child) - 1)
+        # make sure we don't mutate the same child twice
+        if tuple(child) in mutated:
+            continue
 
-        #
-        # 6. Step 6 is to mutate the offspring. You will mutate M of the offpspring, where M is defined
-        # as int(settings.mutation_rate * len(offspring)). Do not mutate a single offspring twice. You
-        # will perform mutation by changing a single element of the individual's array to a random legal
-        # sudoku number, which are given in settings.individual_values
-        M = int(settings.mutation_rate * len(offspring))
-        mutated = set()
+        child[j] = random.choice(settings.individual_values)
 
-        for _ in range(M):
-            i = random.randint(0, len(offspring) - 1)
-            child = offspring[i]
-            j = random.randint(0, len(child) - 1)
+        offspring[i] = child
+        # lists are unhashable so we have to use a tuple (performance impact?)
+        mutated.add(tuple(child))
 
-            if tuple(child) in mutated:
-                continue
-
-            child[j] = random.choice(settings.individual_values)
-
-            offspring[i] = child
-
-            mutated.add(tuple(child))
-
-        next_population.append(offspring)
     # As a possible optimization, you can experiment with performing different types of mutations
     # here, but please add that as an option in your settings file, and explain it in your README.txt
     #
     # 7. Finally, you should combine your elite individual selection with your offspring to form
     # the next population, and return it as a single list. This function will then be called iteratively
     # which each new population. Each iteration is one generation.
+    next_population.extend(offspring)
     return next_population
